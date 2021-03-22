2f20f76d249ce0feda135734b94c0312db22c41b
Leepantam <nighfivego@gmail.com>
2021-03-22 15:18:45 +0900

    0322_2

--- src/test/java/com/leepantam/s2/robot/RobotTest.java
+++ src/test/java/com/leepantam/s2/robot/RobotTest.java
@@ -17,11 +17,14 @@ public class RobotTest {
 	
 	@Autowired
 	private Robot robot;
+	// private Arm_Left arm_Left;
 
 	@Test
-	public void test() {
-		System.out.println(robot.getArm_Left().getName());
-		System.out.println(robot.getArm_Right().getName());
+	public void test(){
+		assertNotNull(robot.getArm_Left());
+		assertNotNull(robot.getArm_Right());
 	}
+	
+
 
 }
